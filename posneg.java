f215fc9d1844a25495d07b6985d6a8c9dd58c2cc
Srinivedha <srinivedhaperiyasamy@gmail.com>
2018-06-18 21:51:39 +0530

    posneg.java

---
+++ posneg.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class posneg
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a==0)
+{
+System.out.print("Zero");
+}
+if(a>0)
+{
+System.out.print("Positive");
+}
+else 
+{
+System.out.print("Negative");
+}
+}
+}
